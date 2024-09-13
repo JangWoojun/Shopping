68cd887b3a01a61003f7639b771ede499802397b
JangWoojun <woojun0107@naver.com>
2024-09-13 10:16:31 +0900

    feat: room, gson 종속성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,6 +41,12 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-android-compiler:2.52")
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    implementation("com.google.code.gson:gson:2.11.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
