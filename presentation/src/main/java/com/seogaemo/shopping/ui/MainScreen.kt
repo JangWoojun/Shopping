1a1e506ce0fb5d15ed17a6e5197951da14f57f1f
JangWoojun <woojun0107@naver.com>
2024-09-13 09:30:04 +0900

    feat: topBar 구현

--- presentation/src/main/java/com/seogaemo/shopping/ui/MainScreen.kt
+++ presentation/src/main/java/com/seogaemo/shopping/ui/MainScreen.kt
@@ -4,16 +4,21 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -21,6 +26,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.seogaemo.shopping.ui.theme.ShoppingTheme
+import com.seogaemo.shopping.viewmodel.MainViewModel
 
 sealed class MainNavigationItem(val route: String, val icon: ImageVector, val name: String) {
     data object Main: MainNavigationItem("Main", Icons.Filled.Home,"Main")
@@ -39,7 +45,11 @@ fun GreetingPreview() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val viewModel = hiltViewModel<MainViewModel>()
     Scaffold(
+        topBar = {
+            Header(viewModel)
+        },
         bottomBar = {
             MainBottomNavigationBar(navController)
         },
@@ -47,6 +57,16 @@ fun MainScreen() {
     ) { innerPadding ->
         MainNavigationScreen(navController, innerPadding)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header(viewModel: MainViewModel) {
+    TopAppBar(title = { Text(text = "My App") }, actions = {
+        IconButton(onClick = { viewModel.openSearchForm() }) {
+            Icon(Icons.Filled.Search, "search icon")
+        }
+    })
 }
 
 @Composable
